13d98f6f7b84bf2d2d67b003769a2bf534dee7b0
Chanda Hubbard <chanda.hubbard@gmail.com>
2020-06-17 18:49:21 -0700

    update

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -22,7 +22,9 @@ notesRouter
       .then((notes) => {
         res.json(notes.map(serializeNotes));
       })
-      // .catch(next);
+      .catch((error) => {
+        console.log(error)
+      })
   })
   .post((req, res, next) => {
     const newNote = req.body;
@@ -43,7 +45,7 @@ notesRouter
           .location(`/notes/${notes.id}`)
           .json(serializeNotes(notes));
       })
-      // .catch(next);
+      .catch(next);
   });
 
 notesRouter
@@ -63,7 +65,7 @@ notesRouter
         res.notes = notes;
         next();
       })
-      // .catch(next);
+      .catch(next);
   })
   .get((req, res, next) => {
     res.json(serializeNotes(res.notes));
@@ -78,7 +80,7 @@ notesRouter
           message: `${req.params.id} note has been deleted`
         });
       })
-      // .catch(next);
+      .catch(next);
   })
   .patch((req, res, next) => {
     const noteToUpdate = req.body;
@@ -101,7 +103,7 @@ notesRouter
           message: `${req.params.id} has been updated`
         });
       })
-      // .catch(next);
+      .catch(next);
   });
 
 module.exports = notesRouter;
